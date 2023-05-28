02cc978c871e097738e36c379d80cbb1dba9cae0
ESTEVAN TANGARIFE CORREA <teban1928@gmail.com>
2023-05-28 09:35:06 -0500

    Created fase api

---
+++ managerEmployess.Api/AutoMapper/ConfigurationProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace managerEmployess.Api.AutoMapper
+{
+    public class ConfigurationProfile : Profile
+    {
+
+        public ConfigurationProfile()
+        {
+         
+         
+            
+            #region Employees
+            CreateMap<Employee, CreateEmployeeCommand>().ReverseMap();
+            CreateMap<Employee, UpdateEmployeeCommand>().ReverseMap();
+            #endregion
+
+
+
+
+        }
+    }
---
+++ managerEmployess.Api/Controllers/EmployeesController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace managerEmployess.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeesUseCase _employeesUseCase;
+        private readonly IMapper _mapper;
+
+
+        public EmployeesController(IEmployeesUseCase employeesUseCase, IMapper mapper)
+        {
+            _employeesUseCase = employeesUseCase;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<string> Create_Employees(CreateEmployeesCommand command)
+        {
+            return await _employeesUseCase.CreateEmployeesAsync(_mapper.Map<Employee>(command));
+        }
+
+        //[HttpGet]
+        //public async Task<List<ContentWithDeliveries>> Get_All_Content()
+        //{
+        //    return await _contentUseCase.GetContentsAsync();
+        //}
+
+        //[HttpPut]
+        //public async Task<string> Update_Content(string idContent, UpdateContentCommand command)
+        //{
+        //    return await _contentUseCase.UpdateContentAsync(idContent, _mapper.Map<Content>(command));
+        //}
+
+        //[HttpGet("{idContent}")]
+        //public async Task<ContentWithDelivery> Get_ById_Content(string idContent)
+        //{
+        //    return await _contentUseCase.GetContentByIdAsync(idContent);
+        //}
+
+
+
+
+    }
+}
