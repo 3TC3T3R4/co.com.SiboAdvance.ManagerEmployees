91462885ca2ed6b4e69707b9fd6504f8de49817a
ESTEVAN TANGARIFE CORREA <teban1928@gmail.com>
2023-05-28 09:45:44 -0500

    fase domain

---
+++ managerEmployees.Domain/Entities/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace managerEmployees.Domain.Entities
+{
+    public class Employee
+    {
+
+        public int employees_id { get; set; }
+        public int area_id { get; set; }
+        public string typeDocument { get; set; }
+        public int number_ID { get; set; }
+        public string name { get; set; } 
+        public string lastName { get; set; }
+
+        public Employee()
+        {
+        }
+
+
+
+
+    }
+}
--- managerEmployess.Api/Controllers/EmployeesController.cs
+++ managerEmployess.Api/Controllers/EmployeesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using managerEmployees.Domain.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace managerEmployess.Api.Controllers
